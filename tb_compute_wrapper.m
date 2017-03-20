88e1c20d96ef49c1b6ad8a2e9ea6f38267887a06
Jingyang Zhou <jyz205@nyu.edu>
2017-03-20 17:16:23 -0400

    .

--- tb_compute_wrapper.m
+++ tb_compute_wrapper.m
@@ -8,23 +8,24 @@
 
 %%
 
-nRepeats = 10;
+nRepeats = 100;
 
 durs      = [2, 4, 8, 16, 32, 64];
-levels    = [0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.8, 1];
+%levels    = [0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.8, 1];
+levels    =  logspace(log10(.01), log10(1), 20); 
 sz1       = [40, 40];
 accumWind = 'exponential';
 rdRule    = 'maxRsp';
 
-for k = 1 : nRepeats
+parfor k = 1 : nRepeats
     [ctr1{k}, coh1{k}] = tb_compute(durs, levels, sz1, accumWind, rdRule);
 end
 
 %% big spatial stimulus
 
-sz2 = [50, 50];
+sz2 = [100, 100];
 
-for k = 1 : nRepeats
+parfor k = 1 : nRepeats
     [ctr2{k}, coh2{k}] = tb_compute(durs, levels, sz2, accumWind, rdRule);
 end
 
@@ -60,13 +61,17 @@ for k = 1 : 2
    stimsz{k}.cohdec =  stimsz{k}.coh/nRepeats;
 end
 
-idx = [1, 3];
-figure
-for k = 1 : 2
-   subplot(2, 2, idx(k))
-   imagesc(stimsz{k}.ctrdec)
-   
-   subplot(2, 2, idx(k) + 1)
-   imagesc(stimsz{k}.cohdec)
-end
+%% save output
 
+saveLoc = fullfile(tb_rootPath, 'output');
+save(fullfile(saveLoc, 'stimsz'), 'stimsz')
+
+%%
+
+% idx = [1, 3];
+% figure
+% for k = 1 : 2
+%    subplot(2, 2, idx(k))
+%    imagesc(stimsz{k}.ctrdec)
+%    
+%    subplot(2, 2,
