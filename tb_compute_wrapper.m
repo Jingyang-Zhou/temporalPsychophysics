d913d689cc6373d50e18f2c0082bcef3ed5efc8d
Jingyang Zhou <jyz205@nyu.edu>
2017-03-21 10:44:25 -0400

    .

--- tb_compute_wrapper.m
+++ tb_compute_wrapper.m
@@ -4,11 +4,11 @@
 
 % for hpc
 addpath(genpath(fullfile(tb_rootPath)))
-shCompileMex
+%shCompileMex
 
 %%
 
-nRepeats = 100;
+nRepeats = 1;
 
 durs      = [2, 4, 8, 16, 32, 64];
 %levels    = [0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.8, 1];
@@ -23,7 +23,7 @@ end
 
 %% big spatial stimulus
 
-sz2 = [100, 100];
+sz2 = [40, 40];
 
 parfor k = 1 : nRepeats
     [ctr2{k}, coh2{k}] = tb_compute(durs, levels, sz2, accumWind, rdRule);
@@ -31,12 +31,12 @@ end
 
 %%  get output
 
-% saveLoc = fullfile(tb_rootPath, 'output');
-% 
-% save(fullfile(saveLoc, 'ctr1'), 'ctr1', '-v7.3')
-% save(fullfile(saveLoc, 'ctr2'),  'ctr2',  '-v7.3')
-% save(fullfile(saveLoc, 'coh1'), 'coh1',  '-v7.3')
-% save(fullfile(saveLoc, 'coh2'), 'coh2', '-v7.3')
+saveLoc = fullfile(tb_rootPath, 'output');
+
+save(fullfile(saveLoc, 'ctr1'), 'ctr1', '-v7.3')
+save(fullfile(saveLoc, 'ctr2'),  'ctr2',  '-v7.3')
+save(fullfile(saveLoc, 'coh1'), 'coh1',  '-v7.3')
+save(fullfile(saveLoc, 'coh2'), 'coh2', '-v7.3')
 
 %% analyze response
 
