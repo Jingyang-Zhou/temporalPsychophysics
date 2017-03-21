9ad63f6b9eff701bbdf614cf8f8d0c59bedf24cd
Jingyang Zhou <jyz205@nyu.edu>
2017-03-21 10:55:43 -0400

    .

--- tb_compute_wrapper.m
+++ tb_compute_wrapper.m
@@ -11,8 +11,8 @@ shCompileMex
 nRepeats = 100;
 
 durs      = [2, 4, 8, 16, 32, 64];
-%levels    = [0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.8, 1];
-levels    =  logspace(log10(.01), log10(1), 20); 
+levels    = [0.01, 0.02, 0.05, 0.1, 0.2, 0.4, 0.8, 1];
+%levels    =  logspace(log10(.01), log10(1), 20); 
 sz1       = [40, 40];
 accumWind = 'exponential';
 rdRule    = 'maxRsp';
